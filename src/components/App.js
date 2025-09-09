15125d3dceeca280b5ea2fe0c1ceca559e749b88
Kapil Singh <kapex.coder@gmail.com>
2025-09-09 19:34:37 +0000

    Submitting: For test

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState(null);
+
+  const handleNameInput = (name) => {
+    setName(name);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <label for="name" >Enter your name:</label> <br />
+      <input name="name" onChange={(e) => handleNameInput(e.target.value.trim())} />
+      <br />
+      {name && <p>Hello {name}!</p>}
     </div>
   )
 }
